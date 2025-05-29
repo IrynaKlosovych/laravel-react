0f11727b0caede228333a7f59d82aebea7d137ba
Iryna Klosovych <ipz221_kia@student.ztu.edu.ua>
2025-05-29 10:35:26 +0300

    header

--- react/src/views/baseLayouts/Header.jsx
+++ react/src/views/baseLayouts/Header.jsx
@@ -1,11 +1,40 @@
 import styles from "./../../css/baseLayouts/Header.module.css"
+import { Link, Navigate } from "react-router-dom"
+import axiosClient from "../../axios-client"
+import { useStateContext } from "../../contexts/StateContext";
 
-export default function Header(){
-    return(
+
+export default function Header() {
+    const { token, setUser, setToken } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />
+    }
+
+    const onLogout = ev => {
+        ev.preventDefault()
+
+        axiosClient.post('/logout')
+            .then(() => {
+                setUser({})
+                setToken(null)
+            })
+    }
+
+    return (
         <header>
-            <div id="logo"></div>
-            <div></div>
-            <div></div>
+            <div className={styles.logoBox}><Link to="/" className={styles.logo}></Link></div>
+
+            <div className={styles.list}>Search inspire</div>
+
+            <div className={styles.rightSection}>
+                <div className={styles.list}>
+                    <Link to="/settings" className={styles.link}>Settings</Link>
+                </div>
+                <div className={styles.list}>
+                    <a href="#" onClick={onLogout} className={styles.link}>LogOut</a>
+                </div>
+            </div>
         </header>
     )
 }
